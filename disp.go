814c4c1562392c59f17687494a6199207e844f2d
Simon Eisenmann <simon@longsleep.org>
2016-05-05 22:13:40 +0200

    Make this go getable.

---
+++ disp.go
@@ -0,0 +1,10 @@
+// Copyright 2016 Simon Eisenmann <simon@longsleep.org>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package disp
+
+import (
+	_ "github.com/longsleep/sunxi-disp-tool/disp2"
+	_ "github.com/longsleep/sunxi-disp-tool/fb"
+)
